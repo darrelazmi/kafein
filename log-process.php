536705f3a1067fbc2e0e9a18f9c2ebb8b5fc7bfb
darrelazmi <darrelat@gmail.com>
2023-11-17 09:56:46 +0700

    masih belum beres juga

--- log-process.php
+++ log-process.php
@@ -1,19 +1,26 @@
 <?php
+    session_start();
     include('config.php');
 
     if (isset($_POST['login'])) {
-        
         $username = $_POST['username'];
         $password = $_POST['password'];
         $query;
 
         if ($_POST['type']=='owner') {
-            $query = mysqli_query("SELECT 'password' FROM 'owner' WHERE 'username' = '$username' OR 'email' = '$username'");
-            $data = mysqli_fetch_array()
+            $query = mysqli_query("SELECT 'owner_id','password' FROM 'owner' WHERE 'username' = '$username' OR 'email' = '$username'");
+            $data = mysqli_fetch_array($query);
+            if ($data['password']==$password) {
+                $_SESSION['id']=$data['owner_id'];
+            }
         }
 
         elseif ($_POST['type']=='customer') {
-            $query = mysqli_query("INSERT INTO `customer`(`name`, `username`, `password`, `email`, `profile_photo`) VALUES ('$name','$username','$password','$email','$profile')");
+            $query = mysqli_query("SELECT 'owner_id','password' FROM 'owner' WHERE 'username' = '$username' OR 'email' = '$username'");
+            $data = mysqli_fetch_array($query);
+            if ($data['password']==$password) {
+                $_SESSION['id']=$data['owner_id'];
+            }
         }
 
         else {
