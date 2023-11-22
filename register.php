01f7016a291a8cdb08a798db1f7489a9fd77bdb8
ZakyGhoetti <139734907+ZakyGhoetti@users.noreply.github.com>
2023-11-23 03:31:33 +0700

    Update register.php
    tambahan error status (kalau belum diisi semua)

--- register.php
+++ register.php
@@ -42,17 +42,26 @@
 		</fieldset>
 	</form>
 
+	<?php if(isset($_GET['status'])): ?>
+	<p>
+		<?php
+			if($_GET['status'] == '1'){
+				echo "Semua kolom harus terisi.<br>\n";
+			}
+		?>
+	</p>
+	<?php endif; ?>
 	<?php if(isset($_GET['username']) && isset($_GET['email'])): ?>
 	<p>
 		<?php
 			if($_GET['username'] == 'false'){
-				echo "Username sudah terdaftar";
+				echo "Username sudah terdaftar.<br>\n";
 			}
 			if($_GET['email'] == 'false'){
-				echo "Email sudah terdaftar";
+				echo "Email sudah terdaftar.<br>\n";
 			}
 			if($_GET['username'] == 'false' || $_GET['email'] == 'false'){
-				echo "Silahkan coba kembali";
+				echo "Silahkan coba kembali.<br>\n";
 			}
 		?>
 	</p>
