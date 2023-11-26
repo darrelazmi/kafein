1d1ac76fc7c9b68854090366d9dae231e323c39e
darrelazmi <darrelat@gmail.com>
2023-11-27 00:18:25 +0700

    Bagian atas beres

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+	include("config.php");
 	session_start();
 	if (isset($_SESSION["id"])) {
 		$id = $_SESSION["id"];
 		$type = $_SESSION["type"];
+		if($type=="owner") $query = mysqli_query($connect,"SELECT `username` FROM `owner` WHERE `owner_id` = '$id'");
+		elseif ($type=="customer") $query =	mysqli_query($connect,"SELECT `username` FROM `customer` WHERE `customer_id` = '$id'");
+		else header("Location: error.php");
+		$username = mysqli_fetch_array($query);
 	}
 ?>
 <!DOCTYPE html>
@@ -13,10 +18,12 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 	<title>Kaffein</title>
+	<link rel="stylesheet" href="./assets/css/styles.css">
 </head>
 <body>
-	<nav class="navbar navbar-expand-sm bg-dark navbar-dark" style="--bs-bg-opacity: .3;">
-		<div class="container-fluid">
+	<img class="bg-img" src="./assets/img/home1.png">
+	<nav class="navbar navbar-expand-sm bg-black navbar-dark" style="--bs-bg-opacity: .3;">
+		<div class="container">
 			<a class="navbar-brand" href="index.php">KAFFEIN</a>
 			<?php if(!isset($_SESSION['id'])): ?>
 			<ul class="navbar-nav">
@@ -29,45 +36,18 @@
 			</ul>
 			<?php endif; ?>
 			<?php if(isset($_SESSION['id'])): ?>
-			<a class="navbar-brand" href="index.php">
-				<img src="./profiles/<?php echo $type; ?>/<?php echo $id; ?>.png" alt="Avatar Logo" style="width:40px;" class="rounded-pill">
-			</a>
-			<ul class="navbar-nav">
-				<a class="navbar-brand" href="index.php">
-					<img src="" alt="">
-				</a>
+			<ul class = "navbar-nav ms-auto">
 				<li class="nav-item">
-					<a href=""></a>
+					<a class="navbar-link" href="profile.php">
+						<img src="./profiles/<?php echo $type; ?>/<?php echo $id; ?>.jpg" alt="Avatar Logo" style="width:40px;height:40px;" class="rounded-pill">
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="profile.php">Welcome, <?php echo $username[0]; ?><a></span>
 				</li>
 			</ul>
 			<?php endif; ?>
-			</ul>
 		</div>
 	</nav>
-	<div class="container-fluid">
-
-	</div>
 </body>
 </html>
-
-
-<!--
-<?php if(isset($_GET['status'])): ?>
-	<p>
-		<?php
-			if($_GET['status'] == 'sukses'){
-				echo "Pendaftaran siswa baru berhasil!";
-			} 
-			else if($_GET['status'] == 'suksesedit'){
-				echo "Edit berhasil!";
-			}
-			else if($_GET['status'] == 'gagaledit'){
-				echo "Edit gagal!";
-			}
-			else {
-				echo "Pendaftaran gagal!";
-			}
-		?>
-	</p>
-<?php endif; ?>
--->
--- register.php
+++ register.php
@@ -32,10 +32,6 @@
 			<label for="email">Email: </label>
 			<input type="text" name="email" placeholder="enter here" />
 		</p>
-        <p>
-			<label for="profile">Profile: </label>
-			<input type="file" name="profile" placeholder="enter here" />
-		</p>
 		<p>
 			<input type="submit" value="Register" name="register" />
 		</p>
