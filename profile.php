83ff17f38162199b44284a0ab5bebddb4e8d5f57
Zaky-G <lyx29ghoetti@apps.ipb.ac.id>
2023-11-30 16:09:23 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -34,7 +34,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Profile</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+	<title>Kaffein</title>
+	<link rel="stylesheet" href="./assets/css/styles.css">
 </head>
 
 <body>
@@ -45,7 +50,7 @@
 		<p>
             <img src='./profiles<?php 
 			echo "/".$type."/".$data['profile_photo'].""
-			?>.jpg' alt="<?php echo $data['name']?>">
+			?>.jpg' alt="<?php echo $data['name']?>" style="height: 250px; width: 250px;" class="rounded-pill" >
         </p>
 		<p>
 			<label for="nama">Name: <?php echo $data['name']?></label>
